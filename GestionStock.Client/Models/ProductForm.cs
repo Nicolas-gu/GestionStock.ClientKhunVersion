d5e5712ac57ff22c03c169fad9eece0c1502bbf7
Khun <lykhun@gmail.com>
2026-01-15 13:56:28 +0100

    add validation

--- GestionStock.Client/Models/ProductForm.cs
+++ GestionStock.Client/Models/ProductForm.cs
@@ -5,15 +5,15 @@ namespace GestionStock.Client.Models
     public class ProductForm
     {
         // validation client
-        [Required]
-        [MaxLength(255)]
+        [Required(ErrorMessage = "Ce champs est requis")]
+        [MaxLength(255, ErrorMessage = "Trop long")]
         public required string Name { get; set; }
 
         public string? Description { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ce champs est requis")]
         public decimal Price { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Ce champs est requis")]
         public int Quantity { get; set; }
         public IEnumerable<int> Categories { get; set; } = [4];
     }
